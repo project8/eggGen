c95f7a93ab3eabaf65eebc3454c7ed013b5156de
Prajwal Mohanmurthy <prajwal@prajwal-Satellite-L755D.(none)>
2014-03-03 21:52:12 -0500

    Beta1

---
+++ CMakeFiles/eggGen.o.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/eggGen.o.dir/eggGen.cxx.o"
+  "eggGen.o.pdb"
+  "eggGen.o"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/eggGen.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
